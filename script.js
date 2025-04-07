209d8ef0161596d6f62fec94273c05dd9557c66b
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-07 22:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 //your JS code here. If required.
-let inputT=document.getElementById("title");
-let inputA=document.getElementById("author");
-let inputI=document.getElementById("isbn");
+
 
 document.getElementById("btn").addEventListener("click",()=>{
-	console.log(inputT.value)
+    let inputT=document.getElementById("title").value;
+    let inputA=document.getElementById("author").value;
+    let inputI=document.getElementById("isbn").value;
+	
+
+	let tablerow=document.createElement("tr");
+	[inputT,inputA,inputI].forEach((val)=>{
+		let td=document.createElement("td");
+		td.textContent=val;
+		tablerow.appendChild(td);
+        // console.log(td.textContent);
+	});
+
+    let del= document.createElement("td");
+    let delBtn= document.createElement("button");
+    delBtn.textContent="X";
+    // del.textContent=delBtn;
+    del.appendChild(delBtn);
+    tablerow.appendChild(del);
+
+    delBtn.addEventListener("click",()=>{
+        tablerow.remove();
+    })
+
+
+    // console.log(document.querySelector("tbody"))
+    document.querySelector("tbody").appendChild(tablerow);
+	
 });
 
 
