b70cae771f1d0552f766b05ebc0245176e700bbd
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-07 21:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let inputT=document.getElementById("title");
+let inputA=document.getElementById("author");
+let inputI=document.getElementById("isbn");
+
+document.getElementById("btn").addEventListener("click",()=>{
+	console.log(inputT.value)
+});
+
+
+
